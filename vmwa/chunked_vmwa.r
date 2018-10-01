1cfb7a560a1672b026496232a34957c40793a2cd
surh <sur00mx@gmail.com>
2018-10-01 16:23:07 -0700

    Fixing License

--- vmwa/chunked_vmwa.r
+++ vmwa/chunked_vmwa.r
@@ -2,20 +2,20 @@
 
 # (C) Copyright 2018 Sur Herrera Paredes
 # 
-# This file is part of RosetteDetector.
+# This file is part of Anchurus
 # 
-# RosetteDetector is free software: you can redistribute it and/or modify
+# Anchurus is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# RosetteDetector is distributed in the hope that it will be useful,
+# Anchurus is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with RosetteDetector.  If not, see <http://www.gnu.org/licenses/>.
+# along with Anchurus.  If not, see <http://www.gnu.org/licenses/>.
 
 library(ggplot2)
 # library(plyr)
