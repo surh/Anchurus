b3eb040e773820bff2d4fa2aada08c10def50175
surh <sur00mx@gmail.com>
2019-10-04 10:36:30 -0700

    adding message about modification

--- strain_phylo/concatenate_alignments.py
+++ strain_phylo/concatenate_alignments.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# NOTE: Do not modify directly. Modify version in micropopgen.
+
 import argparse
 import os
 # import align_markers as am
