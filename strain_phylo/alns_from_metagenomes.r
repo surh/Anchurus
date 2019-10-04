5feb999bf146932ef7517f59f9292c865c0bfdb3
surh <sur00mx@gmail.com>
2019-10-04 16:26:01 -0700

    more printing

--- strain_phylo/alns_from_metagenomes.r
+++ strain_phylo/alns_from_metagenomes.r
@@ -227,6 +227,10 @@ for(midas_dir in args$midas_dir){
 
     row.names(cov) <- genes
     cov <- cov[ rowSums(cov >= args$min_cov) >= (ncol(cov) * args$min_cov), drop = FALSE ]
+    cat("=============\n")
+    cat("dim(cov)", dim(cov), "\n")
+    cat(class(cov), "\n")
+    cat("=============\n\n")
     cov <- cov[ , colSums(cov >= args$min_cov) >= (nrow(cov) * args$min_cov), drop = FALSE ]
 
     # Write selected alignments
