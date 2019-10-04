77bd00e5b118b64117558e37c88c75385b62a751
surh <sur00mx@gmail.com>
2019-10-04 10:45:00 -0700

    addign note about modification

--- strain_phylo/filter_alignment.py
+++ strain_phylo/filter_alignment.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# NOTE: Do not modify directly. Modify version in micropopgen.
+
 import argparse
 import numpy as np
 from Bio import AlignIO
