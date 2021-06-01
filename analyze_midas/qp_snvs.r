4044763aaeac579d4e8b94cbbd1fdf40421f0b97
sur <sur00mx@gmail.com>
2021-06-01 16:44:25 -0700

    Fizing copyright

--- analyze_midas/qp_snvs.r
+++ analyze_midas/qp_snvs.r
@@ -1,20 +1,20 @@
 #!/usr/bin/env Rscript
 
 # (C) Copyright 2020 Sur Herrera Paredes
-#
 # This file is part of Anchurus.
-#
+# 
 # Anchurus is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
+# 
 # Anchurus is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public Licens
+# 
+# You should have received a copy of the GNU General Public License
+# along with Anchurus.  If not, see <https://www.gnu.org/licenses/>.
 
 library(argparser)
 library(HMVAR)
